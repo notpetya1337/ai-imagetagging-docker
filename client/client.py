cb528384c82291a0447dae3531c1fae4915d840f
NotPetya <86024311+notpetya1337@users.noreply.github.com>
2023-10-23 13:58:48 -0600

    Updated to use PaddleOCR

--- client/client.py
+++ client/client.py
@@ -1,8 +1,8 @@
 import concurrent.futures
 import datetime
 import json
-import os
 import logging
+import os
 import sys
 import threading
 import time
@@ -38,12 +38,13 @@ videocollection.create_index("content_md5")
 videocollection.create_index("vision_tags")
 
 # Initialize variables
-imagecount, videocount, queuedimagecount, queuedvideocount = 0, 0, 0, 0
+imagecount, videocount, queuedimagecount, queuedvideocount, exifcount = 0, 0, 0, 0, 0
 foldercount = 0
 imagecount_lock = threading.Lock()
 videocount_lock = threading.Lock()
 queuedimagecount_lock = threading.Lock()
 queuedvideocount_lock = threading.Lock()
+exifcount_lock = threading.Lock()
 
 REDIS_CLIENT = Redis(host=config.redishost, port=config.redisport, db=0)
 
@@ -54,23 +55,24 @@ videomd5s = set()
 for x in videocollection.find({"content_md5": {"$exists": True}}, {"content_md5": 1, "_id": 0}):
     if isinstance(x["content_md5"], list):
         for y in x["content_md5"]: videomd5s.add(y)
-    else: videomd5s.add(x["content_md5"])
-deepbmd5s = set([x["md5"] for x in collection.find({"deepbtags": {"$exists": True}}, {"md5": 1, "_id": 0})])
+    else:
+        videomd5s.add(x["content_md5"])
+# TODO: fix these so entries with `null` aren't added to the set
+deepbmd5s = set(
+    [x["md5"] for x in collection.find({"deepbtags": {"$exists": True, "$type": "array"}}, {"md5": 1, "_id": 0})])
+paddlemd5s = set(
+    [x["md5"] for x in collection.find({"paddleocrtext": {"$exists": True, "$type": "string"}}, {"md5": 1, "_id": 0})])
 visionmd5s = set([x["md5"] for x in collection.find({"vision_tags": {"$exists": True}}, {"md5": 1, "_id": 0})])
 explicitmd5s = set([x["md5"] for x in collection.find({"explicit_detection": {"$exists": True}}, {"md5": 1, "_id": 0})])
 logger.info("Loaded md5s from MongoDB")
 
 
 def push(key, value):
-    REDIS_CLIENT.rpush(key, value)
-
-
-def pull(key):
-    return REDIS_CLIENT.blpop(key)
+    REDIS_CLIENT.lpush(key, value)
 
 
 def process_image_folder(workingdir, is_screenshot, subdiv):
-    global imagecount, imagecount_lock, queuedimagecount, queuedimagecount_lock
+    global imagecount, imagecount_lock, queuedimagecount, queuedimagecount_lock, exifcount, exifcount_lock
     try:
         workingimages = listimages(workingdir, config.process_images)
     except FileNotFoundError as e:
@@ -80,31 +82,51 @@ def process_image_folder(workingdir, is_screenshot, subdiv):
     # "Process only new" loop here
     if config.process_only_new:
         for imagepath in workingimages:
-            with imagecount_lock: imagecount += 1
+            with imagecount_lock:
+                imagecount += 1
             process_models = []
             im_md5 = get_image_md5(imagepath)
             if im_md5 not in allmd5s:
-                if subdiv in config.deepbdivs: process_models.append("deepb"), deepbmd5s.add(im_md5)
+                if subdiv in config.deepbdivs and "deepb" in config.configmodels: process_models.append(
+                    "deepb"), deepbmd5s.add(im_md5)
                 if "vision" in config.configmodels: process_models.append("vision"), visionmd5s.add(im_md5)
+                if "paddleocr" in config.configmodels: process_models.append("paddleocr"), paddlemd5s.add(im_md5)
                 if process_models:
-                    push("queue", json.dumps({"type": 'image', "path": imagepath, "is_screenshot": is_screenshot,
-                                              "subdiv": subdiv, "models": process_models}))
-                with queuedimagecount_lock: queuedimagecount += 1
-            print(f"Processed {imagecount} images with {queuedimagecount} new ", end="\r")
-        # "Process all" loop here
+                    push("queue", json.dumps(
+                        {"type": 'image', "op": "recognition", "path": imagepath, "is_screenshot": is_screenshot,
+                         "subdiv": subdiv, "models": process_models}))
+                with queuedimagecount_lock:
+                    queuedimagecount += 1
+            print(f"Processed {imagecount} images with {queuedimagecount} new ", end="\r")  # "Process all" loop here
     elif not config.process_only_new:
         for imagepath in workingimages:
-            with imagecount_lock: imagecount += 1
+            with imagecount_lock:
+                imagecount += 1
             process_models = []
             im_md5 = get_image_md5(imagepath)
-            if "vision" in config.configmodels and im_md5 not in visionmd5s: process_models.append("vision"), visionmd5s.add(im_md5)
-            if "vision" in config.configmodels and im_md5 not in explicitmd5s: process_models.append("explicit"), explicitmd5s.add(im_md5)
-            if subdiv in config.deepbdivs and im_md5 not in deepbmd5s: process_models.append("deepb"), deepbmd5s.add(im_md5)
-            if process_models is not None:
-                push("queue", json.dumps({"type": 'image', "path": imagepath, "is_screenshot": is_screenshot,
-                                          "subdiv": subdiv, "models": process_models}))
+            if "vision" in config.configmodels and im_md5 not in visionmd5s:
+                process_models.append("vision"), visionmd5s.add(im_md5)
+            if "vision" in config.configmodels and im_md5 not in explicitmd5s:
+                process_models.append("explicit"), explicitmd5s.add(im_md5)
+            if "paddleocr" in config.configmodels and im_md5 not in paddlemd5s:
+                process_models.append("paddleocr"), paddlemd5s.add(im_md5)
+            if subdiv in config.deepbdivs and im_md5 not in deepbmd5s: process_models.append("deepb"), deepbmd5s.add(
+                im_md5)
+            if len(process_models) > 0:
+                push("queue", json.dumps(
+                    {"type": 'image', "op": "recognition", "path": imagepath, "is_screenshot": is_screenshot,
+                     "subdiv": subdiv, "models": process_models}))
                 with queuedimagecount_lock: queuedimagecount += 1
-            print(f"Processed {imagecount} images with {queuedimagecount} new ", end="\r")
+            print(f"Processed {imagecount} images with {queuedimagecount} updated ", end="\r")
+    print("")
+
+    if config.write_exif:
+        for imagepath in workingimages:
+            with exifcount_lock: exifcount += 1
+            push("queue", json.dumps({"type": 'image', "op": "write_exif", "path": imagepath, "subdiv": subdiv,
+                                      "models": config.configmodels}))
+            print(f"Queued EXIF writing for {exifcount} images ", end="\r")
+    print("")
 
 
 def process_video_folder(workingdir, subdiv):
@@ -115,31 +137,39 @@ def process_video_folder(workingdir, subdiv):
     if config.process_only_new:
         for videopath in workingvideos:
             process_models = []
-            with videocount_lock: videocount += 1
+            with videocount_lock:
+                videocount += 1
             vid_md5 = get_video_content_md5(videopath)
             if vid_md5 not in videomd5s:
                 if "vision" in config.configmodels: process_models.append("vision"), videomd5s.add(vid_md5)
             if process_models:
                 with queuedvideocount_lock: queuedvideocount += 1
-                push("queue", json.dumps({"type": 'video', "path": videopath, "subdiv": subdiv, "models": process_models}))
+                push("queue", json.dumps({"type": 'video', "op": "recognition", "path": videopath, "subdiv": subdiv,
+                                          "models": process_models}))
             print(f'Processed {videocount} videos with {queuedvideocount} new', end="\r")
     # Process all videos loop here
     elif not config.process_only_new:
         for videopath in workingvideos:
             process_models = []
-            with videocount_lock: videocount += 1
+            with videocount_lock:
+                videocount += 1
             vid_md5 = get_video_content_md5(videopath)
-            if "vision" in config.configmodels and vid_md5 not in visionmd5s: process_models.append("vision"), visionmd5s.add(vid_md5)
-            if "vision" in config.configmodels and vid_md5 not in explicitmd5s: process_models.append("explicit"), explicitmd5s.add(vid_md5)
-            if subdiv in config.deepbdivs and vid_md5 not in deepbmd5s: process_models.append("deepb"), deepbmd5s.add(vid_md5)
+            if "vision" in config.configmodels and vid_md5 not in visionmd5s: process_models.append(
+                "vision"), visionmd5s.add(vid_md5)
+            if "vision" in config.configmodels and vid_md5 not in explicitmd5s: process_models.append(
+                "explicit"), explicitmd5s.add(vid_md5)
+            if subdiv in config.deepbdivs and vid_md5 not in deepbmd5s: process_models.append("deepb"), deepbmd5s.add(
+                vid_md5)
             if process_models:
                 logger.info("Processing video %s", videopath)
-                push("queue", json.dumps({"type": 'video', "path": videopath, "subdiv": subdiv, "models": process_models}))
+                push("queue", json.dumps({"type": 'video', "op": "recognition", "path": videopath, "subdiv": subdiv,
+                                          "models": process_models}))
             print(f'Processed {videocount} videos with {queuedvideocount} new', end="\r")
 
 
 def main():
-    global foldercount
+    global foldercount, imagecount, videocount, queuedimagecount, queuedvideocount, exifcount
+    foldercount, imagecount, videocount, queuedimagecount, queuedvideocount, exifcount = 0, 0, 0, 0, 0, 0
     start_time = time.time()
     pool = concurrent.futures.ThreadPoolExecutor(max_workers=config.threads)
     for div in config.subdivs:
@@ -162,8 +192,8 @@ def main():
     pool.shutdown(wait=True)
     elapsed_time = time.time() - start_time
     final_time = str(datetime.timedelta(seconds=elapsed_time))
-    logger.warning("All entries processed. Root divs: %s, Folder count: %s, Image count: %s, Video count: %s",
-                   config.subdivs, foldercount, imagecount, videocount)
+    logger.info("All entries processed. Root divs: %s, Folder count: %s, Image count: %s, Video count: %s",
+                config.subdivs, foldercount, imagecount, videocount)
     print(queuedimagecount, "images and ", queuedvideocount, "videos queued.")
     print("Processing took ", final_time)
 
@@ -171,4 +201,7 @@ def main():
 if __name__ == "__main__":
     while True:
         main()
-        time.sleep(600)
+        if config.sleeptime == 0:
+            logger.info("Only running once.")
+            exit(0)
+        time.sleep(config.sleeptime)
--- server/server.py
+++ server/server.py
@@ -4,6 +4,7 @@ import os
 import sys
 import threading
 
+import exiftool
 import pymongo
 from redis import Redis
 
@@ -34,14 +35,14 @@ if "deepb" in config.configmodels:
     threshold = config.deepbthreshold
     deepb_tagger = deepb.deepdanbooruModel(threshold, modelpath, tagfile)
 
-if "easyocr" in config.configmodels:
-    import easyocr
-    reader = easyocr.Reader(['en'], gpu=False)
+if "paddleocr" in config.configmodels:
+    import paddleocr
+    ocr = paddleocr.PaddleOCR(use_angle_cls=True, lang="en")
 
+# TODO: find out if I need to spawn several of these for multithreading.
+et = exiftool.ExifToolHelper(logger=logging.getLogger(__name__).setLevel(logging.INFO), encoding="utf-8", )
 # Initialize variables
-tagging = Tagging(
-    config.google_credentials, config.google_project, tags_backend="google-vision"
-)
+tagging = Tagging(config.google_credentials, config.google_project, tags_backend="google-vision")
 imagecount = 0
 videocount = 0
 foldercount = 0
@@ -49,15 +50,7 @@ foldercount = 0
 imagecount_lock = threading.Lock()
 videocount_lock = threading.Lock()
 # Names of likelihood from google.cloud.vision.enums
-likelihood_name = (
-    "UNKNOWN",
-    "Very unlikely",
-    "Unlikely",
-    "Possible",
-    "Likely",
-    "Very likely",
-)
-
+likelihood_name = ("UNKNOWN", "Very unlikely", "Unlikely", "Possible", "Likely", "Very likely",)
 
 # initialize logger
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
@@ -70,61 +63,45 @@ REDIS_CLIENT = Redis(host=config.redishost, port=config.redisport, db=0)
 
 
 def pull(key):
-    return REDIS_CLIENT.blpop(key)
+    return REDIS_CLIENT.brpop(key)
 
 
-def process_image(imagepath, workingcollection, subdiv, is_screenshot, models):
+def recognize_image(imagepath, workingcollection, subdiv, is_screenshot, models):
     # TODO: handle update/create in redis instead of checking, support getting explicit tags
     im_md5 = get_image_md5(imagepath)
     image_content = get_image_content(imagepath)
     imagepath_array = [imagepath]
     # Check if entry exists in MongoDB, then create new entry or update entry by MD5
-    entry = collection.find_one(
-        {"md5": im_md5},
-        {
-            "md5": 1,
-            "vision_tags": 1,
-            "vision_text": 1,
-            "deepbtags": 1,
-            "explicit_detection": 1,
-        },
-    )
+    entry = collection.find_one({"md5": im_md5},
+                                {"md5": 1, "vision_tags": 1, "vision_text": 1, "deepbtags": 1, "explicit_detection": 1})
     if entry is None:
-        mongo_entry = create_imagedoc(
-            image_content, im_md5, imagepath_array, is_screenshot, subdiv, models
-        )
+        mongo_entry = create_imagedoc(image_content, im_md5, imagepath_array, is_screenshot, subdiv, models)
         workingcollection.insert_one(mongo_entry)
-        logger.info(
-            "Added new entry in MongoDB for image %s: %s\n", imagepath, mongo_entry
-        )
+        logger.info("Added new entry in MongoDB for image %s: %s\n", imagepath, mongo_entry)
     else:
         # Make sure tagging doesn't run twice
-        if "deepb" in models and entry.get("deepbtags") is None:
+        if "deepb" in models and (entry.get("deepbtags") is None or len(entry.get("deepbtags")) == 0):
             logger.info("Processing DeepB tags for image %s", imagepath)
             deepbtags = deepb_tagger.classify_image(imagepath)
-            collection.update_one(
-                {"md5": im_md5}, {"$set": {"deepbtags": deepbtags[1]}}
-            )
-        elif "deepb" in models and len(entry.get("deepbtags")) == 0:
-            logger.info("Processing DeepB tags for image %s", imagepath)
-            deepbtags = deepb_tagger.classify_image(imagepath)
-            collection.update_one(
-                {"md5": im_md5}, {"$set": {"deepbtags": deepbtags[1]}}
-            )
-
+            collection.update_one({"md5": im_md5}, {"$set": {"deepbtags": deepbtags[1]}})
         if "vision" in models and entry.get("vision_tags") is None:
             # TODO: always check MongoDB before processing with Vision because of expense
-            logger.warning("Not processing vision tags yet")
-            # collection.update_one()
-        if "deepdetect" in models and entry["deepdetect_tags"] is None:
-            logger.warning("Not processing deepdetect tags yet")
-            # collection.update_one()
-    # logger.info("Updated MongoDB entry for image %s \n", imagepath)
+            logger.warning("Not processing vision tags yet")  # collection.update_one()
+        if "paddleocr" in models and entry.get("paddleocrtext") is None:
+            logger.info("Processing PaddleOCR text for image %s", imagepath)
+            result = ocr.ocr(imagepath, cls=True)
+            ocrtext = ""
+            for idx in range(len(result)):
+                res = result[idx]
+                if res is not None:
+                    for line in res:
+                        ocrtext += line[1][0] + " "
+            collection.update_one({"md5": im_md5}, {"$set": {"paddleocrtext": ocrtext}})
 
 
 def create_imagedoc(image_content, im_md5, image_array, is_screenshot, subdiv, models):
     print("Processing:", im_md5, image_array, is_screenshot, subdiv, models)
-    tags, text, safe, deepbtags, explicit_detection = None, None, None, None, None
+    tags, text, easytext, ocrtext, safe, deepbtags, explicit_detection = None, None, None, None, None, None, None
     if "deepb" in models and "deepb" not in config.configmodels:
         logger.error("Client requested DeepB tags but DeepB is disabled in config")
     if "deepb" in models and is_screenshot != 1:
@@ -136,74 +113,104 @@ def create_imagedoc(image_content, im_md5, image_array, is_screenshot, subdiv, m
         if is_screenshot != 1:
             tags = tagging.get_tags(image_binary=image_content)
             safe = tagging.get_explicit(image_binary=image_content)
-            explicit_detection = {
-                "adult": f"{likelihood_name[safe.adult]}",
-                "medical": f"{likelihood_name[safe.medical]}",
-                "spoofed": f"{likelihood_name[safe.spoof]}",
-                "violence": f"{likelihood_name[safe.violence]}",
-                "racy": f"{likelihood_name[safe.racy]}",
-            }
+            explicit_detection = {"adult": f"{likelihood_name[safe.adult]}",
+                                  "medical": f"{likelihood_name[safe.medical]}",
+                                  "spoofed": f"{likelihood_name[safe.spoof]}",
+                                  "violence": f"{likelihood_name[safe.violence]}",
+                                  "racy": f"{likelihood_name[safe.racy]}", }
             explicit_detection = [explicit_detection][0]
-    if "deepdetect" in models:
-        logger.warning("Not processing deepdetect tags yet")
-
-    mongo_entry = {
-        "md5": im_md5,
-        "vision_tags": tags,
-        "vision_text": text,
-        "explicit_detection": explicit_detection,
-        "deepbtags": deepbtags,
-        "path": image_array,
-        "subdiv": subdiv,
-        "is_screenshot": is_screenshot,
-    }
-
+    if "paddleocr" in models:
+        result = ocr.ocr(image_array[0], cls=True)
+        for idx in range(len(result)):
+            res = result[idx]
+            if res is not None:
+                ocrtext = ""
+                for line in res:
+                    ocrtext += line[1][0] + " "
+    mongo_entry = {"md5": im_md5, "vision_tags": tags, "vision_text": text, "explicit_detection": explicit_detection,
+                   "deepbtags": deepbtags, "easyocrtext": easytext, "paddleocrtext": ocrtext, "path": image_array,
+                   "subdiv": subdiv, "is_screenshot": is_screenshot}
     logger.info("Generated MongoDB entry: %s", mongo_entry)
     return mongo_entry
+
+
+def mongo_image_data(imagepath, workingcollection, models):
+    """
+    :param imagepath:
+    :param workingcollection:
+    :param models: str
+        models to get tags from, e.g. "vision", "deepb", "explicit"
+        only reads from MongoDB
+    :return:
+    """
+    im_md5 = get_image_md5(imagepath)
+    tagslist, textlist = [], []
+    if "vision" in models:
+        visiontext = json.loads(json.dumps(workingcollection.find_one({"md5": im_md5}, {"vision_text": 1, "_id": 0})))
+        visiontagsjson = json.loads(
+            json.dumps(workingcollection.find_one({"md5": im_md5}, {"vision_tags": 1, "_id": 0})))
+        if visiontagsjson is not None: tagslist.extend(visiontagsjson["vision_tags"])
+        if visiontext is not None: textlist.extend(visiontext["vision_text"])
+    if "deepb" in models:
+        deepbtags = json.loads(json.dumps(workingcollection.find_one({"md5": im_md5}, {"deepbtags": 1, "_id": 0})))
+        if deepbtags is not None: tagslist.extend(deepbtags["deepbtags"])
+    if "explicit" in models:
+        explicit_mongo = workingcollection.find_one({"md5": im_md5}, {"explicit_detection": 1, "_id": 0})
+        if explicit_mongo:
+            detobj = explicit_mongo["explicit_detection"]
+            explicit_results = (
+                f"Adult: {detobj['adult']}", f"Medical: {detobj['medical']}", f"Spoofed: {detobj['spoofed']}",
+                f"Violence: {detobj['violence']}", f"Racy: {detobj['racy']}")
+            tagslist.extend(explicit_results)
+    text = " ".join(textlist)
+    if "paddleocr" in models:
+        paddleocrtext = json.loads(json.dumps(workingcollection.find_one({"md5": im_md5}, {"paddleocrtext": 1, "_id": 0})))
+        if text == "" and paddleocrtext is not None:
+            text = paddleocrtext["paddleocrtext"]
+    text = (text[:config.maxlength] + " truncated...") if len(text) > config.maxlength else text
+    text = text.replace("\n", " ")
+    return tagslist, text
+
+
+def write_image_exif(imagepath, workingcollection, models):
+    tags, text = mongo_image_data(imagepath, workingcollection, models)
+    if tags and text:
+        try:
+            # TODO: check back and see if -ec is necessary
+            et.set_tags(imagepath, tags={"Subject": tags, "xmp:Title": text}, params=["-P", "-overwrite_original"])
+        except exiftool.exceptions.ExifToolExecuteError as e:
+            logger.warning('Error "%s " writing tags, Exiftool output was %s', e, et.last_stderr)
+    elif tags:
+        try:
+            et.set_tags(imagepath, tags={"Subject": tags}, params=["-P", "-overwrite_original"])
+        except exiftool.exceptions.ExifToolExecuteError as e:
+            logger.warning('Error "%s " writing tags, Exiftool output was %s', e, et.last_stderr)
 
 
 def process_video(videopath, workingcollection, subdiv, models, rootdir=""):
     video_content_md5 = str(get_video_content_md5(videopath))
     # TODO: update to check explicit_detection
-    entry = videocollection.find_one(
-        {"content_md5": video_content_md5},
-        {
-            "content_md5": 1,
-            "vision_tags": 1,
-            "vision_text": 1,
-            "vision_transcript": 1,
-            "explicit_detection": 1,
-        },
-    )
+    entry = videocollection.find_one({"content_md5": video_content_md5},
+                                     {"content_md5": 1, "vision_tags": 1, "vision_text": 1, "vision_transcript": 1,
+                                      "explicit_detection": 1})
     if "vision" in models and entry is None:
         relpath = os.path.relpath(videopath, rootdir)
         video_content = get_video_content(videopath)
         vidpath_array = [videopath]
         relpath_array = [relpath]
-        mongo_entry = create_videodoc(
-            video_content, video_content_md5, vidpath_array, relpath_array, subdiv
-        )
+        mongo_entry = create_videodoc(video_content, video_content_md5, vidpath_array, relpath_array, subdiv)
         workingcollection.insert_one(mongo_entry)
         logger.info("Added new entry in MongoDB for video %s \n", videopath)
     else:
-        logger.info("MongoDB entry for video %s already exists: %s \n", videopath, entry)
+        logger.info("MongoDB entry for video %s already exists\n", videopath)
 
 
-def create_videodoc(
-    video_content, video_content_md5, vidpath_array, relpath_array, subdiv
-):
+def create_videodoc(video_content, video_content_md5, vidpath_array, relpath_array, subdiv):
     videoobj = VideoData(config.google_credentials, config.google_project)
     videoobj.video_vision_all(video_content)
-    mongo_entry = {
-        "content_md5": video_content_md5,
-        "vision_tags": videoobj.labels,
-        "vision_text": videoobj.text,
-        "vision_transcript": videoobj.transcripts,
-        "explicit_detection": videoobj.pornography,
-        "path": vidpath_array,
-        "subdiv": subdiv,
-        "relativepath": relpath_array,
-    }
+    mongo_entry = {"content_md5": video_content_md5, "vision_tags": videoobj.labels, "vision_text": videoobj.text,
+                   "vision_transcript": videoobj.transcripts, "explicit_detection": videoobj.pornography,
+                   "path": vidpath_array, "subdiv": subdiv, "relativepath": relpath_array, }
     return mongo_entry
 
 
@@ -212,21 +219,16 @@ while True:
     job = json.loads(pull("queue")[1])
     if job["type"] == "image":
         print("Processing image, job is", job)
-        if job["subdiv"] == "screenshot" and job['models'] is not None:
-            process_image(
-                job["path"],
-                screenshotcollection,
-                job["subdiv"],
-                job["is_screenshot"],
-                job["models"],
-            )
-        elif job['models'] is not None: process_image(
-                job["path"],
-                collection,
-                job["subdiv"],
-                job["is_screenshot"],
-                job["models"],
-            )
-    elif job["type"] == "video" and job['models'] is not None:
-        print("Processing video, job is", job)
-        process_video(job["path"], videocollection, job["subdiv"], job["models"])
+        if job["op"] == "recognition":
+            if job["subdiv"] == "screenshot" and job['models'] is not None:
+                recognize_image(job["path"], screenshotcollection, job["subdiv"], job["is_screenshot"], job["models"], )
+            elif job['models'] is not None:
+                recognize_image(job["path"], collection, job["subdiv"], job["is_screenshot"], job["models"], )
+        if job["op"] == "write_exif":
+            print("Writing EXIF for", job["path"])
+            write_image_exif(job["path"], collection, job["models"])
+
+    elif job["type"] == "video":
+        if job["op"] == "recognition" and job['models'] is not None:
+            print("Processing video, job is", job)
+            process_video(job["path"], videocollection, job["subdiv"], job["models"])
